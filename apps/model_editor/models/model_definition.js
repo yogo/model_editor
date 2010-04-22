c2d8881ec13d51ad98c8e18d370d5ef0cde566cc
Ryan Heimbuch <rheimbuch@gmail.com>
2010-04-22 15:16:42 -0600

    Generated a sproutcore model for definitions of YogoModels.

---
+++ apps/model_editor/models/model_definition.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   ModelEditor.ModelDefinition
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals ModelEditor */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+ModelEditor.ModelDefinition = SC.Record.extend(
+/** @scope ModelEditor.ModelDefinition.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/model_editor/tests/models/model_definition.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   ModelEditor.ModelDefinition Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals ModelEditor module test ok equals same stop start */
+
+module("ModelEditor.ModelDefinition");
+
+// TODO: Replace with real unit test for ModelDefinition
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
