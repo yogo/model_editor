a509b28f224d6319966f74e2b397fc16eb23c8e2
Ryan Heimbuch <rheimbuch@gmail.com>
2010-04-22 15:17:38 -0600

    Added a model for properties in a YogoModel definition.

---
+++ apps/model_editor/models/model_property.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   ModelEditor.ModelProperty
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals ModelEditor */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+ModelEditor.ModelProperty = SC.Record.extend(
+/** @scope ModelEditor.ModelProperty.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/model_editor/tests/models/model_property.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   ModelEditor.ModelProperty Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals ModelEditor module test ok equals same stop start */
+
+module("ModelEditor.ModelProperty");
+
+// TODO: Replace with real unit test for ModelProperty
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
