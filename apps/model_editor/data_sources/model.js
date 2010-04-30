aa2ca418710b6be0123cfc058e1f2f006c01520a
Ryan Heimbuch <rheimbuch@gmail.com>
2010-04-30 14:27:17 -0600

    Implemented the initial datastore to connect to the rails backend

--- apps/model_editor/data_sources/model.js
+++ apps/model_editor/data_sources/model.js
@@ -38,7 +38,6 @@ ModelEditor.ModelDataSource = SC.DataSource.extend(
   
   didFetchModels: function(response, store, query) {
     if(SC.ok(response)) {
-      console.debug(response.get('body'));
       store.loadRecords(ModelEditor.ModelDefinition, response.get('body').models);
       store.dataSourceDidFetchQuery(query);
     }
@@ -53,34 +52,113 @@ ModelEditor.ModelDataSource = SC.DataSource.extend(
   
   retrieveRecord: function(store, storeKey) {
     
-    // TODO: Add handlers to retrieve an individual record's contents
-    // call store.dataSourceDidComplete(storeKey) when done.
+    if(SC.kindOf(store.recordTypeFor(storeKey), ModelEditor.ModelDefinition)) {
+      var modelId = store.idFor(storeKey);
+      var modelUrl = ModelEditor.getPath('yogoConfig.modelsURL') + '/' + modelId;
+      
+      var request = SC.Request.getUrl(url);
+      request.header("Accept","application/json");
+      request.json();
+      request.notify(this, 'didRetrieveModel', store, storeKey);
+      request.send();
+      return YES;
+    }
     
     return NO ; // return YES if you handled the storeKey
+  },
+  
+  didRetrieveModel: function(response, store, storeKey) {
+    if(SC.ok(response)) {
+      var dataHash = response.get('body').Model;
+      store.dataSourceDidComplete(storeKey, dataHash);
+    }
+    else {
+      store.dataSourceDidError(storeKey, response);
+    }
   },
   
   createRecord: function(store, storeKey) {
     
-    // TODO: Add handlers to submit new records to the data source.
-    // call store.dataSourceDidComplete(storeKey) when done.
+    if(SC.kindOf(store.recordTypeFor(storeKey), ModelEditor.ModelDefinition)) {
+      var modelsUrl = ModelEditor.getPath('yogoConfig.modelsURL');
+      var request = SC.Request.postUrl(modelsUrl);
+      request.header('Content-Type','application/json');
+      request.header('Accept', 'application/json')
+      request.json();
+      request.notify(this, 'didCreateModel', store, storeKey);
+      console.debug('createRecord: sending');
+      request.send({"Model": store.readDataHash(storeKey) });
+      return YES;
+    }
     
     return NO ; // return YES if you handled the storeKey
+  },
+  
+  didCreateModel: function(response, store, storeKey) {
+    console.debug('didCreateModel');
+    if(SC.ok(response)) {
+      var dataHash = response.get('body').Model;
+      store.dataSourceDidComplete(storeKey, dataHash);
+    }
+    else {
+      store.dataSourceDidError(storeKey, response);
+    }
   },
   
   updateRecord: function(store, storeKey) {
     
-    // TODO: Add handlers to submit modified record to the data source
-    // call store.dataSourceDidComplete(storeKey) when done.
+    if(SC.kindOf(store.recordTypeFor(storeKey), ModelEditor.ModelDefinition)) {
+      var modelId = store.idFor(storeKey);
+      var modelUrl = ModelEditor.getPath('yogoConfig.modelsURL') + '/' + modelId;
+      
+      var request = SC.Request.putUrl(modelUrl);
+      request.json();
+      request.header('Content-Type','application/json');
+      request.header('Accept', 'application/json');
+      request.notify(this, 'didUpdateModel', store, storeKey);
+      request.send({"Model": store.readDataHash(storeKey)});
+      return YES;
+    }
 
     return NO ; // return YES if you handled the storeKey
   },
   
+  didUpdateModel: function(response, store, storeKey) {
+    if(SC.ok(response)) {
+      var data = response.get('body');
+      data = data && data.Model;
+      store.dataSourceDidComplete(storeKey, data);
+    }
+    else {
+      store.dataSourceDidError(storeKey);
+    }
+  },
+  
   destroyRecord: function(store, storeKey) {
     
-    // TODO: Add handlers to destroy records on the data source.
-    // call store.dataSourceDidDestroy(storeKey) when done
+    if(SC.kindOf(store.recordTypeFor(storeKey), ModelEditor.ModelDefinition)) {
+      var modelId = store.idFor(storeKey);
+      var modelUrl = ModelEditor.getPath('yogoConfig.modelsURL') + '/' + modelId;
+      
+      var request = SC.Request.deleteUrl(modelUrl);
+      request.json();
+      request.header('Content-Type','application/json');
+      request.header('Accept', 'application/json');
+      request.notify(this, 'didDestroyModel', store, storeKey);
+      request.send();
+      return YES;
+    }
     
     return NO ; // return YES if you handled the storeKey
+  },
+  
+  didDestroyModel: function(response, store, storeKey) {
+    if(SC.ok(response)) {
+      store.dataSourceDidDestroy(storeKey);
+    }
+    else {
+      store.dataSourceDidError(response);
+    }
   }
   
 }) ;
